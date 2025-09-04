588b2e8ab9c5e26c453478de28f9e0494e5e94f7
alonsolopezjeronimo66-cpu <alonsolopezjeronimo66@gmail.com>
2025-09-04 13:28:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from PIL import Image
 
-st.title(" Mi Primera App!!")
+st.title("Millonarios FC")
 
-st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
-st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('Interfaces Mult2.png')
+st.header("Conoce al equipo mas grande de colombia.")
+st.write("El famosos ballet azul")
+image = Image.open('MillonariosFC.png')
 
-st.image(image, caption='Interfaces multimodales')
+st.image(image, caption='Escudo')
 
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
